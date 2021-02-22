220942f47a05fcd3e91e947838377107bb9fde01
a.v.gorshkov <a.v.gorshkov@tinkoff.ru>
2021-02-22 19:05:35 +0300

    Spring init

---
+++ src/main/java/ru/itmo/blss/firstlab/FirstLabApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.blss.firstlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itmo/blss/firstlab/FirstLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.blss.firstlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
